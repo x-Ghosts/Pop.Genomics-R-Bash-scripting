b1c59ed33f04bd3a5ebf404efdb3a5b48594348f
xGhosts <78695896+x-Ghosts@users.noreply.github.com>
2026-06-22 15:51:18 +0200

    Update step_1_extract_samples.R

--- step_1_extract_samples.R
+++ step_1_extract_samples.R
@@ -23,34 +23,6 @@ barplot(fid_counts,
 # Count samples per FID
 fid_counts <- table(fam$V1)
 fid_counts <- as.data.frame(fid_counts)
-keep_above_30 <- fid_counts[fid_counts$Freq > 30,]
+keep_above_50 <- fid_counts[fid_counts$Freq > 50,]
 
-write.table(keep_above_30$Var1, "above_30.txt", quote = F, col.names = F, row.names = F)
-
-
-# Keep only populations with > 30 individuals
-pops <- names(fid_counts[fid_counts > 1])
-
-# Define groups
-africa_north <- c("ALG","BAR","BRK","DRA","GHA","MOR","NBN","NDA","NOR","OSS","SID","TUN")
-fr_spain <- c("BEY","CRS","FSS","MLG","PAL","PTV","PVC","PYR","RAS","SP_MGRANA")
-italy_specific <- c("IT_GARFAG")
-
-# Build annotation table
-annotation <- data.frame(
-  FID = pops,
-  N = as.integer(fid_counts[pops]),   # 👈 number of samples
-  stringsAsFactors = FALSE
-)
-
-# Assign regions
-annotation$Region <- "Italy"  # default
-annotation$Region[annotation$FID %in% africa_north] <- "Africa_North"
-annotation$Region[annotation$FID %in% fr_spain] <- "France_Spain"
-annotation$Region[annotation$FID %in% italy_specific] <- "Italy"
-
-# View result
-annotation
-
-
-write.csv(annotation, "dataset.csv", quote = FALSE, sep = ",")
+write.table(keep_above_50$Var1, "above_50.txt", quote = F, col.names = F, row.names = F)
