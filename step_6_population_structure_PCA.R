1ada871e9888b53acd32d807a558be32384ca68b
xGhosts <78695896+x-Ghosts@users.noreply.github.com>
2026-06-23 13:53:23 +0200

    Update step_6_population_structure_PCA.R

--- step_6_population_structure_PCA.R
+++ step_6_population_structure_PCA.R
@@ -91,3 +91,72 @@ print(p)
 path_PCA <- file.path(paste0(plot_output_folder,"/PCA_",pc1,"_",pc2,".png"))
 ggsave(path_PCA, plot = p, width = 16, height = 8, dpi = 300)
 
+#################################################################################################################
+#################################################################################################################
+#################################################################################################################
+#################################################################################################################
+
+# Population Structure based on Continental Split:
+
+african_codes <- c("ALG","BAR","BRK","DRA","GHA","MOR","NBN","NDA","NOR","OSS","SID","TUN","CAM","DJA",
+                   "GUE","MAU","NAI","PEU","RSK","SAH","SDN","SHL","TAR","WAD")
+european_codes <- c("BEY", "CRS", "FSS", "MLG", "PAL", "PTV", "PVC", "PYR","RAS", "SP_MGRANA", "ARG", "ASP",
+                    "BIA", "BIO", "CAP", "DDS", "FAC", "FUL","GAR","GCI", "GIR","IT_GARFAG","JON","LIV","MAL",
+                    "MES", "MLS", "MLT", "MON", "NIC", "NVE", "ORO", "RCC", "RME", "SAR", "TER", "VAL", "VLS","VSS")
+
+
+pc_df <- data.frame(
+  FID = pc_vec$V1,
+  IID = pc_vec$V2,
+  PC1 = pc_vec[, pc1 + 2],
+  PC2 = pc_vec[, pc2 + 2],
+  stringsAsFactors = FALSE
+)
+
+pc_df$Component <- ifelse(
+  pc_df$FID %in% african_codes,
+  "AFRICA",
+  "EUROPE"
+)
+
+centroids <- pc_df %>%
+  group_by(FID, Component) %>%
+  summarise(
+    PC1 = mean(PC1, na.rm = TRUE),
+    PC2 = mean(PC2, na.rm = TRUE),
+    .groups = "drop"
+  )
+
+p <- ggplot(pc_df, aes(x = PC1, y = PC2, color = Component)) +
+  geom_point(size = 2, alpha = 0.9) +
+  geom_label_repel(
+    data = centroids,
+    aes(x = PC1, y = PC2, label = FID, color = Component),
+    fill = "white",
+    fontface = "bold",
+    size = 4,
+    box.padding = 0.35,
+    point.padding = 0.2,
+    label.padding = 0.20,
+    label.size = 0.35,
+    max.overlaps = Inf,
+    show.legend = FALSE
+  ) +
+  scale_color_manual(
+    values = c(
+      "AFRICA" = "red",
+      "EUROPE" = "blue"
+    )
+  ) +
+  labs(
+    title = "PCA of Mediterranean Goats:",
+    x = paste0("PC", pc1, " (", round(var_percent[pc1], 2), "%)"),
+    y = paste0("PC", pc2, " (", round(var_percent[pc2], 2), "%)"),
+    color = "Continent component"
+  ) +
+  theme_minimal(base_size = 14)
+
+print(p)
+
+path_PCA_component <- file.path(paste0(plot_output_folder, "/comp_PCA_", pc1, "_", pc2, ".png"))
+ggsave(path_PCA_component, plot = p, width = 16, height = 8, dpi = 300)
